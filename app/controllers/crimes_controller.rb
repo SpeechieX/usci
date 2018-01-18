85b413057829952f46402ee0f6af7d08c6401dd4
Erik Robertson <writeerikh@gmail.com>
2018-01-18 14:01:29 -0800

    fixed syntax error

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -1,21 +1,23 @@
 class CrimesController < ApplicationController
 
-def show
-    @crimes = Crime.all
-end
-
-def create
-    @crime = Crime.new(crime_params)
-    @crimes.operator = current_user
-    
-    if @crimes.save
-        redirect_to new_criminal_path 
-    else
-        render :new
+    def show
+        @crimes = Crime.all
     end
-end
-private
 
-def crime_params
-    params.require(:crime).permit(:charge, :date_of_charge, :victim, :case_closed)
+    def create
+        @crime = Crime.new(crime_params)
+        @crimes.operator = current_user
+        
+        if @crimes.save
+            redirect_to new_criminal_path 
+        else
+            render :new
+        end
+    end
+    private
+
+    def crime_params
+        params.require(:crime).permit(:charge, :date_of_charge, :victim, :case_closed)
+    end
+
 end
