47867d59aaf85b8b1f080b6ecec819aa7be31754
Erik HR <writeerikh@gmail.com>
2022-08-12 08:43:23 +0700

    makeup

--- app/controllers/operators_controller.rb
+++ app/controllers/operators_controller.rb
@@ -9,7 +9,7 @@ class OperatorsController < ApplicationController
         @operator = Operator.new(operator_params)
         if @operator.save
           session[:operator_id] = @operator.id 
-          flash[:notice] = "You have successfully signed up!"
+          flash[:notice] = "You have successfully signed up!!!!"
           redirect_to criminals_path 
         else
           render :new
